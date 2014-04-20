0b6e2ab2a409749b3d481a8e8006e0e4ef51c111
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2014-04-20 14:18:54 +0200

    exctacted observable, finished game round

--- web/player.dart
+++ web/player.dart
@@ -1,38 +1,53 @@
 part of PokerDart;
 
-class Player implements IHtmlRenderable {
+class Player implements IHtmlRenderable, IObserver {
     String name;
-    List<Card> _cards;
+    ObservableList<Card> _cards;
+    HtmlElement _playerElement;
 
-    Player(this.name, {List<Card> cards: null}) {
+    Player(this.name, {ObservableList<Card> cards: null}) {
         if (cards != null) {
             _cards = cards;
         } else {
-            _cards = new List<Card>();
+            _cards = new ObservableList<Card>();
         }
+        _cards.registerObserver(this);
+
+        _playerElement = new DivElement();
+        _playerElement.classes.add('card-container');
     }
 
-    void addCard(Card c){
+    void addCard(Card c) {
         _cards.add(c);
     }
 
-    void addCards(Iterable<Card> cards){
+    void addCards(Iterable<Card> cards) {
         _cards.addAll(cards);
+    }
+
+    void clearHand() {
+        _cards.clear();
+    }
+
+    void renderHandTo(HtmlElement element){
+        _cards.forEach((c) => element.append(c.render()));
     }
 
     @override
     HtmlElement render() {
         HtmlElement container = new DivElement();
         container.classes.add('player-container');
-        HtmlElement player = new DivElement();
         HtmlElement name = new HeadingElement.h2();
         name.text = this.name;
 
-        player.classes.add('card-container');
-        _cards.forEach((c) => player.append(c.render()));
-
         container.append(name);
-        container.append(player);
+        container.append(_playerElement);
         return container;
+    }
+
+    @override
+    void notify() {
+        _playerElement.innerHtml = '';
+        renderHandTo(_playerElement);
     }
 }
--- web/pokerdart.dart
+++ web/pokerdart.dart
@@ -1,10 +1,10 @@
 library PokerDart;
 import 'dart:html';
+import 'lib/observable.dart';
 part 'player.dart';
 part 'symbol.dart';
 part 'card.dart';
 part 'deck.dart';
-part 'observable_list.dart';
 
 HtmlElement playerElement;
 HtmlElement communityCards;
@@ -28,17 +28,11 @@ void startGame(String playerName) {
 
     querySelector('.jumbotron').classes.add('hidden');
 
-    querySelector('#continue-btn').onClick.listen((e) => playTable.nextStage());
-
     querySelector('body').append(playTable.render());
 }
 
 abstract class IHtmlRenderable {
     HtmlElement render();
-}
-
-abstract class IObserver {
-    void notify();
 }
 
 void handleReady(event) {
@@ -60,22 +54,31 @@ class PlayTable implements IHtmlRenderable, IObserver {
         _communityCards.classes.add('card-container');
 
         _cards = new ObservableList<Card>();
-        _cards.subscribe(this);
+        _cards.registerObserver(this);
 
         _playTable = new DivElement();
         _playTable.classes.add('play-table');
 
-        _continueButton = new ButtonElement();
+        _continueButton = BootstrapFactory.CreateButton('Continue!', BootstrapButtonType.primaryType);
+        _continueButton.onClick.listen(nextStage);
+
+        _playTable.append(_continueButton);
 
         _players.forEach((p) => _playTable.append(p.render()));
 
         _playTable.append(_communityCards);
     }
 
-    void nextStage() {
+    void nextStage(e) {
         switch (_gameStage) {
-            case 1:
+            case 0:
                 _cards.clear();
+                _continueButton.text = 'Continue!';
+                _deck = new Deck();
+                _players.forEach(_dealNewHand);
+                _gameStage++;
+                break;
+            case 1:
                 _cards.addAll(_deck.deal(3));
                 _gameStage++;
                 break;
@@ -85,9 +88,15 @@ class PlayTable implements IHtmlRenderable, IObserver {
                 break;
             case 3:
                 _cards.addAll(_deck.deal());
-                _gameStage = 1;
+                _continueButton.text = 'Next game';
+                _gameStage = 0;
                 break;
         }
+    }
+
+    void _dealNewHand(Player p){
+        p.clearHand();
+        p.addCards(_deck.deal(2));
     }
 
     @override
@@ -116,9 +125,14 @@ class BootstrapButtonType{
 }
 
 class BootstrapFactory{
-    static CreateButtonElement(String text, BootstrapButtonType bbType){
+    static ButtonElement CreateButton(String text, [BootstrapButtonType bbType]){
         var buttonElement = new ButtonElement();
+        if (bbType == null){
+            bbType = BootstrapButtonType.defaultType;
+        }
         buttonElement.classes.add('btn ${bbType.buttonType}');
+        buttonElement.text = text;
+        return buttonElement;
     }
 }
 
